ded25707b5ebd041307f7127eaf1d3380766893a
PC <PC@ANDREJKOVPOITA>
2020-10-30 16:11:30 +0100

    Neviem nasiel som na injurnete

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -6,7 +6,23 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+            int[,] bruh = new int[str1.Length + 1, str2.Length + 1];
+            int pleb1 = 0;    
+            int pleb2 = 0;    
+
+            for (var i = 0; i < str1.Length; i++)
+                for (var j = 0; j < str2.Length; j++)
+                    if (str1[i] == str2[j])
+                    {
+                        int len = bruh[i + 1, j + 1] = bruh[i, j] + 1;
+                        if (len > bruh[pleb1, pleb2])
+                        {
+                            pleb1 = i + 1;
+                            pleb2 = j + 1;
+                        }
+                    }
+
+            return str1.Substring(pleb1 - bruh[pleb1, pleb2], bruh[pleb1, pleb2]);
         }
     }
 }
